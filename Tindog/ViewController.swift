daeecb79c96d220b17d935b6e416634cd5171cbd
0sw4l <ioswxd@gmail.com>
2018-11-11 01:04:52 -0500

    Initial Commit

---
+++ Tindog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tindog
+//
+//  Created by osw4l on 11/11/18.
+//  Copyright © 2018 osw4l. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
